2170df8963435f7f6d584137339fac9815c8ad76
David Hays <dhays90@gmail.com>
2017-12-15 19:53:07 -0600

    hima: camera: SurfaceComposerClient shim
    Change-Id: Ida1f6520c3a506576e8f1f8803ebcfdb730df696

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -32,6 +32,23 @@ LOCAL_PROPRIETARY_MODULE := true
 
 include $(BUILD_SHARED_LIBRARY)
 
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+    camera_hw_shim.cpp
+
+LOCAL_SHARED_LIBRARIES := \
+    libui
+
+LOCAL_MODULE := libshim_camera_hw
+LOCAL_MODULE_TAGS := optional
+LOCAL_MULTILIB := 32
+LOCAL_PROPRIETARY_MODULE := true
+
+include $(BUILD_SHARED_LIBRARY)
+
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
